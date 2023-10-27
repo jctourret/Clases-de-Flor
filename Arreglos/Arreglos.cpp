30561fe5f36a035f866afe77258257f206598af1
jctourret <54895509+jctourret@users.noreply.github.com>
2023-10-26 21:41:54 -0300

    Agregados Arreglos y Funciones

---
+++ Arreglos/Arreglos.cpp
@@ -0,0 +1,33 @@
+﻿#include <iostream>
+
+using namespace std;
+
+int main()
+{   //Es una cadena de variables una al lado de la otra.
+	//TipoDeDato nombreDeVariable[tamaño] = {variable, variable, variable, etc}  
+	char map[100] = { 'c','a','s','i','t','a' };
+
+	//imprimi mi array;
+	cout << map << endl;
+
+	//imprimi el primer espacio de mi array.
+	cout << map[0] << endl;
+
+	for (int i = 0; i < 100; i++)
+	{
+		map[i] = ' ';
+	}
+	getchar();
+
+	for (int i = 0; i < 100; i++)
+	{
+		wcout << map[i];
+	}
+	cout << endl;
+
+	//╔════════╗
+	//╠═══╦════╣
+	//║   ║    ║
+	//╠═══╬════╣
+	//╚═══╩════╝
+}
